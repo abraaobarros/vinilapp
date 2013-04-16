b3305896671f2a0b8276a2da0e73932c545f36f1
Gabriel Chagas Marques <Gabriel@gabriel.rede>
2013-04-16 15:08:19 -0300

    HttpService

---
+++ VinilApp/HttpService.h
@@ -0,0 +1,13 @@
+//
+//  HttpService.h
+//  VinilApp
+//
+//  Created by Gabriel Chagas Marques on 4/16/13.
+//  Copyright (c) 2013 Abraao Barros Laceda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HttpService : NSObject
+
+@end
---
+++ VinilApp/HttpService.m
@@ -0,0 +1,13 @@
+//
+//  HttpService.m
+//  VinilApp
+//
+//  Created by Gabriel Chagas Marques on 4/16/13.
+//  Copyright (c) 2013 Abraao Barros Laceda. All rights reserved.
+//
+
+#import "HttpService.h"
+
+@implementation HttpService
+
+@end
